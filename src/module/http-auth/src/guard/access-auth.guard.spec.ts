c42c48ae4bf6d684d7684c30161e0f4f763ead05
Pooya Azarpour <pooya_azarpour@yahoo.com>
2023-02-25 15:32:59 +0330

    Add feature for ignore auth gaurd

--- src/module/http-auth/src/guard/access-auth.guard.spec.ts
+++ src/module/http-auth/src/guard/access-auth.guard.spec.ts
@@ -1,16 +1,30 @@
-import { AccessAuthGuard } from './access-auth.guard';
 import {Reflector} from '@nestjs/core';
 import {mock, MockProxy} from 'jest-mock-extended';
 import {Test, TestingModule} from '@nestjs/testing';
+import {ExecutionContext} from '@nestjs/common';
+
+let authGuardCanActivate = jest.fn();
+jest.mock('@nestjs/passport', () => {
+  return {
+    AuthGuard: function () {
+      const fn = jest.fn();
+      fn.prototype.canActivate = authGuardCanActivate;
+
+      return fn;
+    },
+  };
+});
+
+import { AccessAuthGuard } from './access-auth.guard';
+
 
 describe('AccessAuthGuard', () => {
   let accessAuthGuard: AccessAuthGuard;
   let reflector: MockProxy<Reflector>;
 
   beforeEach(async () => {
-    reflector = mock<Reflector>();
-
     const reflectorProvider = 'reflector';
+    reflector = mock<Reflector>();
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -32,9 +46,78 @@ describe('AccessAuthGuard', () => {
   afterEach(() => {
     jest.restoreAllMocks();
     jest.resetAllMocks();
+    authGuardCanActivate.mockClear();
   });
 
   it('should be defined', () => {
     expect(accessAuthGuard).toBeDefined();
+  });
+
+  describe(`canActivate`, () => {
+    let disableCheckAuthDecoratorName: string;
+    let context: MockProxy<ExecutionContext>;
+    let contextGetClass: string;
+    let contextGetHandler: string;
+
+    beforeEach(() => {
+      disableCheckAuthDecoratorName = 'disableCheckAuth';
+      context = mock<ExecutionContext>();
+
+      contextGetClass = 'class';
+      contextGetHandler = 'handler';
+    });
+
+    it(`Should successfully return true when is enable ignore check auth decorator on class`, () => {
+      (<jest.Mock>context.getClass).mockReturnValue(contextGetClass);
+      (<jest.Mock>context.getHandler).mockReturnValue(contextGetHandler);
+      (<jest.Mock>reflector.get)
+        .mockReturnValueOnce(true)
+        .mockReturnValueOnce(undefined);
+
+      const result = accessAuthGuard.canActivate(context);
+
+      expect(context.getClass).toHaveBeenCalled();
+      expect(context.getHandler).toHaveBeenCalled();
+      expect(reflector.get).toHaveBeenCalledTimes(2);
+      expect(reflector.get).toHaveBeenNthCalledWith(1, disableCheckAuthDecoratorName, contextGetClass);
+      expect(reflector.get).toHaveBeenNthCalledWith(2, disableCheckAuthDecoratorName, contextGetHandler);
+      expect(result).toBeTruthy();
+    });
+
+    it(`Should successfully return true when is disable ignore check auth decorator on class`, () => {
+      (<jest.Mock>context.getClass).mockReturnValue(contextGetClass);
+      (<jest.Mock>context.getHandler).mockReturnValue(contextGetHandler);
+      (<jest.Mock>reflector.get).mockReturnValue(undefined);
+      authGuardCanActivate.mockReturnValue(true);
+
+      const result = accessAuthGuard.canActivate(context);
+
+      expect(context.getClass).toHaveBeenCalled();
+      expect(context.getHandler).toHaveBeenCalled();
+      expect(reflector.get).toHaveBeenCalledTimes(2);
+      expect(reflector.get).toHaveBeenNthCalledWith(1, disableCheckAuthDecoratorName, contextGetClass);
+      expect(reflector.get).toHaveBeenNthCalledWith(2, disableCheckAuthDecoratorName, contextGetHandler);
+      expect(authGuardCanActivate).toHaveBeenCalled();
+      expect(authGuardCanActivate).toHaveBeenCalledWith(context);
+      expect(result).toBeTruthy();
+    });
+
+    it(`Should successfully return true when is disable ignore check auth decorator on class`, () => {
+      (<jest.Mock>context.getClass).mockReturnValue(contextGetClass);
+      (<jest.Mock>context.getHandler).mockReturnValue(contextGetHandler);
+      (<jest.Mock>reflector.get).mockReturnValue(undefined);
+      authGuardCanActivate.mockReturnValue(false);
+
+      const result = accessAuthGuard.canActivate(context);
+
+      expect(context.getClass).toHaveBeenCalled();
+      expect(context.getHandler).toHaveBeenCalled();
+      expect(reflector.get).toHaveBeenCalledTimes(2);
+      expect(reflector.get).toHaveBeenNthCalledWith(1, disableCheckAuthDecoratorName, contextGetClass);
+      expect(reflector.get).toHaveBeenNthCalledWith(2, disableCheckAuthDecoratorName, contextGetHandler);
+      expect(authGuardCanActivate).toHaveBeenCalled();
+      expect(authGuardCanActivate).toHaveBeenCalledWith(context);
+      expect(result).toBeFalsy();
+    });
   });
 });
--- src/module/http-auth/src/guard/access-auth.guard.ts
+++ src/module/http-auth/src/guard/access-auth.guard.ts
@@ -5,13 +5,20 @@ import {AuthGuard} from '@nestjs/passport';
 
 @Injectable()
 export class AccessAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
-    super(reflector);
+  constructor(private readonly _reflector: Reflector) {
+    super(_reflector);
   }
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const classDisableCheckAuth = this._reflector.get<string[]>('disableCheckAuth', context.getClass());
+    const handlerDisableCheckAuth = this._reflector.get<string[]>('disableCheckAuth', context.getHandler());
+    const disableCheckAuth = classDisableCheckAuth || handlerDisableCheckAuth;
+    if (disableCheckAuth) {
+      return true;
+    }
+
     return super.canActivate(context);
   }
 }
